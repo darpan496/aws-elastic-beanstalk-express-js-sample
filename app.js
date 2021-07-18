3c7c438343b53fbd472a041438f4aeec40cda42d
darpan496 <69737153+darpan496@users.noreply.github.com>
2021-07-17 17:41:45 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Change message'));
+app.get('/', (req, res) => res.send('Change, Commit, etc message'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
